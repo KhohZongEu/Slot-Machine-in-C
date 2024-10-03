97880932d1c8335fe0351972dc05aaea15a5640d
KhohZongEu <khohzongeu@example.com>
2024-10-03 22:05:37 +1000

    added cmake

---
+++ build/CMakeFiles/Slotmachine.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Slotmachine.dir/src/main.c.obj"
+  "CMakeFiles/Slotmachine.dir/src/main.c.obj.d"
+  "Slotmachine.exe"
+  "Slotmachine.exe.manifest"
+  "Slotmachine.pdb"
+  "libSlotmachine.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Slotmachine.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/Slotmachine.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Slotmachine.
+# This may be replaced when dependencies are built.
